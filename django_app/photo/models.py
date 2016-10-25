b8829b78565d5436595d8b3fc0add1f6adbd918f
HyominJang <hyominj92@gmail.com>
2016-10-25 16:32:22 +0900

    photo, album db model 생성

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length=30)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    description = models.CharField(max_length=400, blank=True)
+        # description은 required field가 아니므로 blank = True
+class Photo(models.Model):
+    Album = models.ForeignKey(Album)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=70)
+    description = models.CharField(max_length=400, blank=True)
+    img = models.ImageField(upload_to='photo')
