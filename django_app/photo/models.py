a2bf8f6ea8e1ec94a2a10596b74d6114c2af93cb
HyominJang <hyominj92@gmail.com>
2016-10-25 17:28:33 +0900

    photo의 models에 likeuser dislikeuser필드 추가 및 중간자모델 구현, 해당내용 migration생성:

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -13,3 +13,18 @@ class Photo(models.Model):
     title = models.CharField(max_length=70)
     description = models.CharField(max_length=400, blank=True)
     img = models.ImageField(upload_to='photo')
+    # M2M Field를 사용해서 좋아요 내용을 거치도록 합니다 (중간자 모델)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='PhotoLike', related_name='photo_set_like_users')
+    dislike_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='PhotoDislike', related_name='photo_set_dislike_users')
+# Many to many field에서 중간에 생성되는 table과 같다
+
+class PhotoLike(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class PhotoDislike(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
